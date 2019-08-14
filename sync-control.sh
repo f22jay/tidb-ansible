c66d254de8018d7db412b8c3ee6aebdb12af0784
Cloud User <centos@ip-172-16-4-132.us-east-2.compute.internal>
2019-08-14 02:59:39 +0000

    a little fix.

--- sync-control.sh
+++ sync-control.sh
@@ -3,7 +3,7 @@
 cd ~ && rm -rf control-large.tar
 cd ~ && tar cf control-large.tar control-large
 
-hosts=(tidb02 tidb03 tidb04)
+hosts=(tidb01 tidb02 tidb03 tidb04)
 for h in ${hosts[@]}; do
 	ssh -i ~/.ssh/tmp-samsung-poc.pem centos@$h "rm -rf control-large control-large.tar"
 	scp -i ~/.ssh/tmp-samsung-poc.pem ~/control-large.tar centos@$h:~/
